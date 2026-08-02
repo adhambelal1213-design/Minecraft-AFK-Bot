24ba34812ee174bd9458867e1bc53b32efd5bced
adhambelal1213-design <adhambelal1213@gmail.com>
2026-08-02 18:57:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,8 +2,8 @@ const mineflayer = require('mineflayer');
 const config = require('./config.json');
 
 const bot = mineflayer.createBot({
-  host: config.serverHost,
-  port: config.serverPort,
+  host: config.tnteditor.falixsrv.me,
+  port: config.23613,
   username: config.botUsername,
   auth: 'offline',
   version: false,
